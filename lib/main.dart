069a8354dd017daea6ba1e618ce8eef6a7d24dee
saurabhguptarock <saurabhguptarock119@gmail.com>
2019-09-02 00:24:39 +0530

    Added qpp shortcuts, splashscreen and other pages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,87 @@
+import 'package:Baron/pages/settings_page.dart';
+import 'package:Baron/pages/upgrade_page.dart';
+import 'package:Baron/splash_screen.dart';
+import 'package:Baron/users/login.dart';
+import 'package:Baron/users/profile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:quick_actions/quick_actions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
+
+bool isLoggedIn;
+bool isDark = false;
+
+checkLogin() async {
+  final prefs = await SharedPreferences.getInstance();
+  isLoggedIn = prefs.getBool("isLoggedIn") ?? false;
+}
+
+checkTheme() async {
+  final prefs = await SharedPreferences.getInstance();
+  isDark = prefs.getBool("theme") ?? false;
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Baron',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+      title: 'QuickActions Demo',
+      theme: isDark == true ? ThemeData.dark() : ThemeData.light(),
+      home: QuickActionsManager(),
+      routes: <String, WidgetBuilder>{
+        "/profile": (BuildContext context) => ProfilePage(),
+        "/login": (BuildContext context) => LoginPage(),
+        "/upgrade": (BuildContext context) => UpgradePage(),
+        "/settings": (BuildContext context) => SettingsPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class QuickActionsManager extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _QuickActionsManagerState createState() => _QuickActionsManagerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+class _QuickActionsManagerState extends State<QuickActionsManager> {
+  final QuickActions quickActions = QuickActions();
+  void _setupQuickActions() {
+    quickActions.setShortcutItems(<ShortcutItem>[
+      ShortcutItem(
+          type: 'action_profile', localizedTitle: 'Profile', icon: 'user'),
+      ShortcutItem(
+          type: 'action_upgrade', localizedTitle: 'Upgrade', icon: 'upgrade'),
+      ShortcutItem(
+          type: 'action_settings',
+          localizedTitle: 'Settings',
+          icon: 'settings'),
+    ]);
+  }
+
+  void _handleQuickActions() {
+    quickActions.initialize((shortcutType) {
+      if (shortcutType == 'action_profile') {
+        Navigator.pushNamed(context, '/profile');
+      } else if (shortcutType == 'action_upgrade') {
+        Navigator.pushNamed(context, '/upgrade');
+      } else if (shortcutType == 'action_settings') {
+        Navigator.pushNamed(context, '/settings');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      backgroundColor: Colors.green,
-      drawer: Drawer(
-        child: Text('data'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(80.0),
-        child: InkWell(
-          onTap: () => _scaffoldKey.currentState.openDrawer(),
-          child: SizedBox(
-            height: 20,
-            width: 20,
-            child: SpinKitRing(
-              color: Colors.red,
-              duration: Duration(milliseconds: 800),
-              size: 30,
-              lineWidth: 3,
-            ),
-          ),
-        ),
-      ),
-    );
+    return SplashScreen();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkTheme();
+    checkLogin();
+    _setupQuickActions();
+    _handleQuickActions();
   }
 }
---
+++ lib/users/profile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Profile',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
