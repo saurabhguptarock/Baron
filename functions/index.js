70512f6a09d4b4e9b2fa85c69829d7e1da2395a9
saurabhguptarock <saurabhguptarock119@gmail.com>
2019-09-15 23:24:13 +0530

    Updated firebase functions

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('./services-account.json');
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://baron-260a4.firebaseio.com",
+    storageBucket: "baron-260a4.appspot.com",
+    projectId: "baron-260a4"
+})
+    
+const defaultStorage = admin.storage();
+
+exports.onFileUpload = functions.storage.object().onFinalize(async object => {
+    const fileName = object.name;
+    const bucket = defaultStorage.bucket();
+    const file = bucket.file(object.name);
+    const results = await file
+        .getSignedUrl({
+            action: 'read',
+            expires: '03-17-2025'
+          });
+    const url = results[0];
+    const category = fileName.split('/')[0];
+    let quality = 1;
+    
+    if (category == 'classic')
+        quality = 1
+    else if (category == 'epic')
+        quality=2
+    else if (category == 'rare')
+        quality=3
+    else if (category == 'legendary')
+        quality=4
+    else if (category == 'mythic')
+        quality = 5
+    
+    admin.firestore().collection('collectibles').add({
+            img:url,
+            name:fileName.split('/')[1],
+            quality:quality
+    }).then((id)=>{console.error(id.id)})
+})
